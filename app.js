23c5fbac237a3a458e3fd10247bc8e2faf84c0ad
gary909 <garywhite61@hotmail.com>
2023-06-01 12:29:48 +0200

    code

---
+++ app.js
@@ -0,0 +1,7 @@
+function spacey(array){
+
+}
+
+console.log(spacey(['kevin', 'has','no','space'])); // [ 'kevin', 'kevinhas', 'kevinhasno', 'kevinhasnospace']
+console.log(spacey(['this','cheese','has','no','holes'])); 
+// ['this','thischeese','thischeesehas','thischeesehasno','thischeesehasnoholes']
