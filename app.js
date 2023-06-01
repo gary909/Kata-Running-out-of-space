232d9a4be451e5ed4a919a560dd18805c67f12d4
gary909 <garywhite61@hotmail.com>
2023-06-01 12:35:26 +0200

    code working

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 function spacey(array){
-
+    let myStr = '';
+    let myArr = [];
+    for(let i = 0; i < array.length; i++){
+        myStr = myStr + array[i];
+        myArr.push(myStr);
+    }
+return myArr;
 }
 
 console.log(spacey(['kevin', 'has','no','space'])); // [ 'kevin', 'kevinhas', 'kevinhasno', 'kevinhasnospace']
